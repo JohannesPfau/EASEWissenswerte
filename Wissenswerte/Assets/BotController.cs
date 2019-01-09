340cdde5a11c05551db48828bdde02afffe842c4
m-u-d <jpfau@tzi.de>
2019-01-09 18:14:29 +0100

    EASEWissenswerte initial | EASEY Character | Movement | ArmController

---
+++ Wissenswerte/Assets/BotController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour {
+
+    public float accMult = 0.15f;
+    public float rotMult = 1;
+    public float maxWheelieTilt = 5;
+    public float maxWheelieTiltBack = 1;
+    public float wheelsMult = 5;
+    public Transform[] wheels;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            // velocity
+            transform.position = transform.position + new Vector3(Input.GetAxis("Horizontal") * accMult, 0, 0);
+
+            //spin wheels
+            foreach (Transform wheel in wheels)
+                wheel.localRotation = Quaternion.Euler(wheel.localRotation.eulerAngles + new Vector3(0,0,Input.GetAxis("Horizontal") * wheelsMult));
+
+            // "wheelie" animation effect
+            if (Input.GetAxis("Horizontal") > 0 && transform.localRotation.eulerAngles.z < maxWheelieTilt)
+                transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles + new Vector3(0, 0, Input.GetAxis("Horizontal") * rotMult));
+
+        }
+        
+        //rollover protection
+        if (transform.localRotation.eulerAngles.z > maxWheelieTilt && transform.localRotation.eulerAngles.z < 180)
+            transform.localRotation = Quaternion.Euler(0, 0, maxWheelieTilt);
+        if(transform.localRotation.eulerAngles.z > 180 && transform.localRotation.eulerAngles.z < 360- maxWheelieTiltBack)
+            transform.localRotation = Quaternion.Euler(0, 0, 360- maxWheelieTiltBack);
+    }
+}
---
+++ Wissenswerte/Assets/MoveBotArm.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBotArm : MonoBehaviour {
+
+    public float forceMult = 1;
+    public Rigidbody[] relRigidbodies;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.GetAxis("HorizontalRIGHT");
+        float y = Input.GetAxis("VerticalRIGHT");
+
+        if(x == 0 && y == 0) // if not needed: idle arms
+        {
+            foreach(Rigidbody r in relRigidbodies)
+                r.useGravity = true;
+            return;
+        }
+        foreach (Rigidbody r in relRigidbodies)
+            r.useGravity = false;
+        
+        // move Tool in desired direction
+        foreach (Rigidbody r in relRigidbodies)
+        {
+            r.AddForce(x * forceMult, y * forceMult, 0, ForceMode.Acceleration);
+        }
+    }
+}
